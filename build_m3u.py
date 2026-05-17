7d19b4dd63971c51e808b4d323fbf86c5971f42b
msr-sagor <164005758+msr-sagor@users.noreply.github.com>
2026-05-17 21:48:04 +0600

    Update build_m3u.py

--- build_m3u.py
+++ build_m3u.py
@@ -12,21 +12,33 @@ def main():
     lines = ["#EXTM3U"]
 
     for match in data:
+
         title = match["title"]
-        play_id = match["play_id"]
 
         for s in match["streams"]:
 
             name = f'{title} | {s["name"]}'
-            url = f"https://footsters.livesports18.workers.dev/?play={play_id}&stream={s['stream']}"
+            url = f'https://footsters.livesports18.workers.dev/?play={match["play_id"]}&stream={s["stream"]}'
 
-            lines.append(f'#EXTINF:-1,{name}')
-            lines.append(url)
+            lines.append(f'#EXTINF:-1 tvg-name="{name}",{name}')
+
+            # Only add DRM if available
+            if s["mpd"] and s["clearkey"]["kid"] and s["clearkey"]["key"]:
+
+                lines.append("#KODIPROP:inputstream.adaptive.stream_type=dash")
+                lines.append("#KODIPROP:inputstream.adaptive.license_type=clearkey")
+                lines.append(
+                    f'#KODIPROP:inputstream.adaptive.license_key={s["clearkey"]["kid"]}:{s["clearkey"]["key"]}'
+                )
+
+                lines.append(s["mpd"])
+            else:
+                lines.append(url)
 
     with open(OUTPUT, "w", encoding="utf-8") as f:
         f.write("\n".join(lines))
 
-    print("DONE M3U")
+    print("M3U DONE")
 
 
 if __name__ == "__main__":
